e6aba6a909ae71ea3102c791ebbacbc3367b90da
Maxim Tereshchenko <tereshchenko.xini@gmail.com>
2021-07-27 22:58:41 +0300

    tests of @Log.In, @Log.Out, @Log.Error using SpringAspectAdapter

--- src/test/java/by/geranium/adapter/SpringAspectAdapterTest.java
+++ src/test/java/by/geranium/adapter/SpringAspectAdapterTest.java
@@ -1,5 +1,6 @@
 package by.geranium.adapter;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
@@ -7,8 +8,10 @@ import static org.mockito.Mockito.verifyNoInteractions;
 import by.geranium.annotation.Log;
 import by.geranium.core.Geranium;
 import by.geranium.core.LoggingLevel;
+import by.geranium.core.MethodCall;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatcher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -28,15 +31,100 @@ class SpringAspectAdapterTest {
     private TestClass testClass;
 
     @Test
-    void givenAnnotatedMethod_thenCallMethod_thenAdviceInvoked() throws Throwable {
-        testClass.annotatedMethod();
+    void givenMethodWithLogAnnotation_thenCallMethod_thenAdviceInvoked() throws Throwable {
+        testClass.methodWithLogAnnotation();
 
-        verify(geranium).logMethodCall(argThat(argument -> argument.targetClass() == TestClass.class &&
-                argument.methodArguments().isEmpty() &&
-                argument.returnType() == void.class &&
-                argument.methodName().equals("annotatedMethod") &&
-                argument.exceptionLoggingLevel() == LoggingLevel.OFF &&
-                argument.inLoggingLevel() == LoggingLevel.DEBUG));
+        verify(geranium).logMethodCall(
+                argThat(
+                        methodOfTestClassWithoutArgumentsVoidReturnType(
+                                "methodWithLogAnnotation",
+                                LoggingLevel.INFO,
+                                LoggingLevel.INFO,
+                                LoggingLevel.OFF
+                        )
+                )
+        );
+    }
+
+    @Test
+    void givenMethodWithLogInAnnotation_thenCallMethod_thenAdviceInvoked() throws Throwable {
+        testClass.methodWithLogInAnnotation();
+
+        verify(geranium).logMethodCall(
+                argThat(
+                        methodOfTestClassWithoutArgumentsVoidReturnType(
+                                "methodWithLogInAnnotation",
+                                LoggingLevel.INFO,
+                                LoggingLevel.OFF,
+                                LoggingLevel.OFF
+                        )
+                )
+        );
+    }
+
+    @Test
+    void givenMethodWithLogOutAnnotation_thenCallMethod_thenAdviceInvoked() throws Throwable {
+        testClass.methodWithLogOutAnnotation();
+
+        verify(geranium).logMethodCall(
+                argThat(
+                        methodOfTestClassWithoutArgumentsVoidReturnType(
+                                "methodWithLogOutAnnotation",
+                                LoggingLevel.OFF,
+                                LoggingLevel.INFO,
+                                LoggingLevel.OFF
+                        )
+                )
+        );
+    }
+
+    @Test
+    void givenMethodWithBothLogInOutAnnotations_thenCallMethod_thenAdviceInvoked() throws Throwable {
+        testClass.methodWithBothLogInOutAnnotations();
+
+        verify(geranium).logMethodCall(
+                argThat(
+                        methodOfTestClassWithoutArgumentsVoidReturnType(
+                                "methodWithBothLogInOutAnnotations",
+                                LoggingLevel.WARN,
+                                LoggingLevel.ERROR,
+                                LoggingLevel.OFF
+                        )
+                )
+        );
+    }
+
+    @Test
+    void givenMethodWithBothLogInOutAndPlainLogAnnotations_thenCallMethod_thenAdviceInvoked() throws Throwable {
+        testClass.methodWithBothLogInOutAndPlainLogAnnotations();
+
+        verify(geranium).logMethodCall(
+                argThat(
+                        methodOfTestClassWithoutArgumentsVoidReturnType(
+                                "methodWithBothLogInOutAndPlainLogAnnotations",
+                                LoggingLevel.INFO,
+                                LoggingLevel.INFO,
+                                LoggingLevel.OFF
+                        )
+                )
+        );
+    }
+
+    @Test
+    void givenMethodWithLogErrorAnnotation_thenCallMethod_thenAdviceInvoked() throws Throwable {
+        assertThatThrownBy(() -> testClass.methodWithLogErrorAnnotation())
+                .isInstanceOf(IllegalArgumentException.class);
+
+        verify(geranium).logMethodCall(
+                argThat(
+                        methodOfTestClassWithoutArgumentsVoidReturnType(
+                                "methodWithLogErrorAnnotation",
+                                LoggingLevel.OFF,
+                                LoggingLevel.OFF,
+                                LoggingLevel.INFO
+                        )
+                )
+        );
     }
 
     @Test
@@ -46,10 +134,49 @@ class SpringAspectAdapterTest {
         verifyNoInteractions(geranium);
     }
 
+    private ArgumentMatcher<MethodCall> methodOfTestClassWithoutArgumentsVoidReturnType(
+            String methodName,
+            LoggingLevel inLoggingLevel,
+            LoggingLevel outLoggingLevel,
+            LoggingLevel exceptionLoggingLevel
+    ) {
+        return argument -> argument.targetClass() == TestClass.class &&
+                argument.methodArguments().isEmpty() &&
+                argument.returnType() == void.class &&
+                argument.methodName().equals(methodName) &&
+                argument.inLoggingLevel() == inLoggingLevel &&
+                argument.outLoggingLevel() == outLoggingLevel &&
+                argument.exceptionLoggingLevel() == exceptionLoggingLevel;
+    }
+
     static class TestClass {
 
-        @Log
-        void annotatedMethod() {
+        @Log(LoggingLevel.INFO)
+        void methodWithLogAnnotation() {
+        }
+
+        @Log.In(LoggingLevel.INFO)
+        void methodWithLogInAnnotation() {
+        }
+
+        @Log.Out(LoggingLevel.INFO)
+        void methodWithLogOutAnnotation() {
+        }
+
+        @Log.In(LoggingLevel.WARN)
+        @Log.Out(LoggingLevel.ERROR)
+        void methodWithBothLogInOutAnnotations() {
+        }
+
+        @Log(LoggingLevel.INFO)
+        @Log.In(LoggingLevel.WARN)
+        @Log.Out(LoggingLevel.ERROR)
+        void methodWithBothLogInOutAndPlainLogAnnotations() {
+        }
+
+        @Log.Error(LoggingLevel.INFO)
+        void methodWithLogErrorAnnotation() {
+            throw new IllegalArgumentException();
         }
 
         void methodWithoutAnnotation() {
