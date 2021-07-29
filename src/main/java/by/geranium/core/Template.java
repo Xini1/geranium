7b12ac2a70a263df71f541d8a2e057b83159a6bb
Maxim Tereshchenko <tereshchenko.xini@gmail.com>
2021-07-29 10:43:02 +0300

    Implement actual replacement method in Template

--- src/main/java/by/geranium/core/Template.java
+++ src/main/java/by/geranium/core/Template.java
@@ -2,6 +2,8 @@ package by.geranium.core;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.Map;
+
 /**
  * @author Maxim Tereshchenko
  */
@@ -9,4 +11,14 @@ import lombok.RequiredArgsConstructor;
 public class Template {
 
     private final String original;
+
+    public String replace(Map<SupportedPlaceholders, String> placeholderMap) {
+        return placeholderMap.entrySet()
+                .stream()
+                .reduce(
+                        original,
+                        (accumulated, entry) -> accumulated.replace(entry.getKey().placeholder(), entry.getValue()),
+                        (first, second) -> first //parallel streams are not supported
+                );
+    }
 }
