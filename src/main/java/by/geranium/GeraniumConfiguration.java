f3071c80873fee57e904b0002ebe2b2747f5a77a
Maxim Tereshchenko <tereshchenko.xini@gmail.com>
2021-07-29 10:07:28 +0300

    Add pattern configuration method in GeraniumConfiguration

--- src/main/java/by/geranium/GeraniumConfiguration.java
+++ src/main/java/by/geranium/GeraniumConfiguration.java
@@ -3,6 +3,7 @@ package by.geranium;
 import by.geranium.core.Geranium;
 import by.geranium.core.Logger;
 import by.geranium.core.LoggingStrategyFactory;
+import by.geranium.core.Template;
 import by.geranium.core.ValueSerializingStrategy;
 
 import java.util.ArrayList;
@@ -15,6 +16,9 @@ public class GeraniumConfiguration {
 
     private final List<ValueSerializingStrategy> valueSerializingStrategyList = new ArrayList<>();
     private LoggingStrategyFactory loggingStrategyFactory;
+    private Template inLoggingTemplate;
+    private Template outLoggingTemplate;
+    private Template throwableLoggingTemplate;
 
     public GeraniumConfiguration withLoggingStrategyFactory(LoggingStrategyFactory loggingStrategyFactory) {
         this.loggingStrategyFactory = loggingStrategyFactory;
@@ -26,11 +30,27 @@ public class GeraniumConfiguration {
         return this;
     }
 
+    public GeraniumConfiguration withInLoggingPattern(String pattern) {
+        inLoggingTemplate = new Template(pattern);
+        return this;
+    }
+
+    public GeraniumConfiguration withOutLoggingPattern(String pattern) {
+        outLoggingTemplate = new Template(pattern);
+        return this;
+    }
+
+    public GeraniumConfiguration withThrowableLoggingPattern(String pattern) {
+        throwableLoggingTemplate = new Template(pattern);
+        return this;
+    }
+
     public Geranium build() {
         return new Geranium(
                 new Logger(
                         loggingStrategyFactory,
-                        valueSerializingStrategyList
+                        valueSerializingStrategyList,
+                        inLoggingTemplate, outLoggingTemplate, throwableLoggingTemplate
                 )
         );
     }
