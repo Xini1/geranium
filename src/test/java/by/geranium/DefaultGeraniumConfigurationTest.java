3a25098f1f0c997894d076cb5ac2c5fc6c0181d3
Maxim Tereshchenko <tereshchenko.xini@gmail.com>
2021-08-03 11:35:27 +0300

    Test for default log in pattern

---
+++ src/test/java/by/geranium/DefaultGeraniumConfigurationTest.java
@@ -0,0 +1,56 @@
+package by.geranium;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import by.geranium.annotation.Log;
+import by.geranium.core.ToStringSerializingStrategy;
+import by.geranium.core.VoidReturnTypeSerializingStrategy;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Maxim Tereshchenko
+ */
+class DefaultGeraniumConfigurationTest {
+
+    private LoggingStrategyStub loggingStrategyStub;
+
+    @BeforeEach
+    void setUp() {
+        loggingStrategyStub = new LoggingStrategyStub();
+    }
+
+    @Test
+    void givenDefaultLogInPattern_thenLogWithExpectedFormat() {
+        TestConfiguration.forInterface(TestInterface.class)
+                .forObject(new TestClass())
+                .withGeraniumConfiguration(
+                        new GeraniumConfiguration()
+                                .withLoggingStrategyFactory(new LoggingStrategyFactoryStub(loggingStrategyStub))
+                                .withValueSerializingStrategy(new VoidReturnTypeSerializingStrategy())
+                                .withValueSerializingStrategy(new ToStringSerializingStrategy())
+                                .withDefaultInLoggingPattern()
+                                .withOutLoggingPattern("")
+                                .withThrowableLoggingPattern("")
+                )
+                .build()
+                .methodWithLogAnnotation("input");
+
+        assertThat(loggingStrategyStub.getMessages())
+                .contains("DEBUG methodWithLogAnnotation > str = input");
+    }
+
+    private interface TestInterface {
+
+        String methodWithLogAnnotation(String str);
+    }
+
+    public static class TestClass implements TestInterface {
+
+        @Log
+        @Override
+        public String methodWithLogAnnotation(String str) {
+            return "return value";
+        }
+    }
+}
